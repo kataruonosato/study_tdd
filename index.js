26a7b315515d271e41f8715ca807a39e372a2866
kataruonosato <dankatakata@gmail.com>
2023-02-03 23:41:11 +0900

    feature: #1 1を渡すと文字列"1"を返す関数を仮実装した

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function add(a, b) {
-    return a + b;
+function fizzBuzz(num) {
+    return '1';
 }
 
-module.exports = add;
+module.exports = fizzBuzz;
--- index.test.js
+++ index.test.js
@@ -1,9 +1,6 @@
-const add = require('./index')
+const fizzBuzz = require('./index')
 
 
 test("1を渡すと文字列'1'を返す", ()=> {
-    // 準備(Arrange)
-    // 実行(Act)
-    // 検証(Assertion)
-    expect(actual).toBe('1');
+    expect(fizzBuzz(1)).toBe('1');
 });
